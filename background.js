9c12158e6819d3710a52dee7c9912e5697a4ffd3
578101568@qq.com <578101568@qq.com>
2020-03-01 15:59:43 -0800

    send to server(fake)

--- background.js
+++ background.js
@@ -1,24 +1,24 @@
-// console.log('hello background')
-function scrollMap(position) {
-    // Scrolls the map so that it is centered at
-    //  (position.coords.latitude, position.coords.longitude).
+function sendData(position) {
     console.log(position);
-  }
-  
-  // Request repeated updates.
-  //var watchId = navigator.geolocation.watchPosition(scrollMap);
+    $.ajax({
+        type: "GET",
+        url: "http://www.yourwebsite.com/",
+        data: { getData: position },
+        success: function(data){
+          // ...
+        }
+      });
 
-//reference: https://levelup.gitconnected.com/how-to-use-background-script-to-fetch-data-in-chrome-extension-ef9d7f69625d
+
+  }
 chrome.runtime.onInstalled.addListener(() => {
-    // create alarm after extension is installed / upgraded
     chrome.alarms.create('refresh', { periodInMinutes: 1 });
   });
   
   chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log(alarm.name); // refresh
     helloWorld();
   });
   
   function helloWorld() {
-    var watchId = navigator.geolocation.watchPosition(scrollMap);
+    navigator.geolocation.watchPosition(sendData);
   }
