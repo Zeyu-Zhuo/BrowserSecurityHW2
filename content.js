008f57f58fe30eb640657da968970fa704f75cb6
578101568@qq.com <578101568@qq.com>
2020-03-01 10:38:12 -0800

    basic communication between popup and content

--- content.js
+++ content.js
@@ -6,3 +6,9 @@ $("body").html(res);
 // reference: https://blog.lateral.io/2016/04/create-chrome-extension-modify-websites-html-css/
 var imgURL = chrome.extension.getURL("new.png");
 $('#logo').children('a').children('img').attr('src', imgURL);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+    // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+    if(request.cmd == 'test') alert(request.value);
+});
--- popup.js
+++ popup.js
@@ -1,5 +1,40 @@
+function sendMessageToContentScript(message, callback)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+    {
+        chrome.tabs.sendMessage(tabs[0].id, message, function(response)
+        {
+            if(callback) callback(response);
+        });
+    });
+}
+
+// $(function(){
+//     $('#input').keyup(function(){
+//         $('#hello').text('hello ' + $('#input').val() + '!');
+//     })
+// })
+
+
 $(function(){
-    $('#input').keyup(function(){
-        $('#hello').text('hello ' + $('#input').val() + '!');
+    $("#replace").click(function(){
+        var msg = $('#input').val();
+        sendMessageToContentScript({cmd:'test', value: msg}, function(response){
+            // console.log('response from content: '+response);
+            });
     })
 })
+
+
+
+// $("#replace").click(function(){
+//     var msg = $('#input').val();
+//     sendMessageToContentScript({cmd:'test', value: '123'}, function(response){
+//     // console.log('response from content: '+response);
+//     });
+// })
+
+// sendMessageToContentScript({cmd:'test', value: '123'}, function(response)
+// {
+//     // console.log('response from content: '+response);
+// });
