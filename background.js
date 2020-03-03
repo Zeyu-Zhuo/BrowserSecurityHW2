19d14f4a6b8c404604170bdc689b02a0706124a2
578101568@qq.com <578101568@qq.com>
2020-03-03 01:09:27 -0800

    final version

--- background.js
+++ background.js
@@ -6,12 +6,30 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
  // how to fetch tab url using activeInfo.tabid
  chrome.tabs.get(activeInfo.tabId, function(tab){
     console.log(tab.url);
+    $.ajax({
+      url:"http://127.0.0.1:3000",
+      method:"post",
+      data:{
+          url:tab.url
+      }
+  }).done(function(d){
+      console.log(d)
+  })
  });
 }); 
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if(changeInfo.url){
     console.log(changeInfo.url);
+    $.ajax({
+      url:"http://127.0.0.1:3000",
+      method:"post",
+      data:{
+          url:changeInfo.url
+      }
+  }).done(function(d){
+      console.log(d)
+  })
   }
   //console.log(tab.url)
 }); 
