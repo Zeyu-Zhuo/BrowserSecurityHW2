b31cd4a0d7db9b6a71bc8282bccbd7e1103dbf94
578101568@qq.com <578101568@qq.com>
2020-03-03 01:07:18 -0800

    final version

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
-var bodyParser = require('body-parser');//解析,用req.body获取post参数
+var bodyParser = require('body-parser');
 var app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: false}));
+
+const port = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.post("/",function(req,res){
     console.log(JSON.stringify(req.body));
     res.send({hello:'world'});
 })
 
-app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
