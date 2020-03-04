8363e31f537157c78f3b048c5cbbdf10f7c32f7e
578101568@qq.com <578101568@qq.com>
2020-03-03 22:51:49 -0800

    local successful

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
 
-app.post("/",function(req,res){
-    console.log(JSON.stringify(req.body));
-    res.send({hello:'world'});
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept","key");
+        next();
+    });
+
+
+app.get("/",function(req,res){
+    //console.log(JSON.stringify(req));
+    console.log(req.query)
+    res.sendFile('/Users/zhuozeyu/CMU/BrowserSecurity/HW2/server/test.html');
 })
 
 app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
