54ce05cc67de76ba389d5fde6ae1d05c16f44c41
578101568@qq.com <578101568@qq.com>
2020-03-03 18:31:47 -0800

    on the way

--- background.js
+++ background.js
@@ -2,34 +2,40 @@
 
 // }); 
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
- // how to fetch tab url using activeInfo.tabid
- chrome.tabs.get(activeInfo.tabId, function(tab){
-    console.log(tab.url);
-    $.ajax({
-      url:"http://127.0.0.1:3000",
-      method:"post",
-      data:{
-          url:tab.url
-      }
-  }).done(function(d){
-      console.log(d)
-  })
- });
-}); 
-
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
+//  // how to fetch tab url using activeInfo.tabid
+//  chrome.tabs.get(activeInfo.tabId, function(tab){
+//     console.log(tab.url);
+//  });
+// }); 
+titles = {};
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(changeInfo.url){
-    console.log(changeInfo.url);
-    $.ajax({
-      url:"http://127.0.0.1:3000",
-      method:"post",
-      data:{
-          url:changeInfo.url
-      }
-  }).done(function(d){
-      console.log(d)
-  })
+  if (changeInfo.title) {
+    console.log(changeInfo.title)
+    if (!titles[tabId]) {
+      titles[tabId] = changeInfo.title
+    } else if (titles[tabId] === changeInfo.title) {
+      // do nothing
+    } else { 
+      chrome.tabs.query({}, function(tabs) {
+        console.log(tabs)
+        tabs.forEach(t => {
+          if (t.id != tabId) {
+            chrome.tabs.executeScript(t.id, {
+              // hacky way to include jQuery
+              file: 'jquery-3.4.1.min.js'
+            }, function() {
+              chrome.tabs.executeScript(t.id, 
+                {code: `var title = "${changeInfo.title}"`}
+              , function() {
+                console.log(1234)
+                chrome.tabs.executeScript(t.id, 
+                  {file: 'insert_update.js'});
+                })
+           });
+          }
+        })
+      });
+    }
   }
-  //console.log(tab.url)
 }); 
