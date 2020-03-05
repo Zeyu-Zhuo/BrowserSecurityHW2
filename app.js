9875932164774a53266bc04122440bdf9763b6c0
578101568@qq.com <578101568@qq.com>
2020-03-04 16:48:42 -0800

    final version t2

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
-
+var dataToWrite;
+var fs = require('fs');
+var dataToWrite;
 const port = 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post("/",function(req,res){
-    console.log(JSON.stringify(req.body));
-    res.send({hello:'world'});
+    dataToWrite = req.body.url + ','
+    fs.appendFile('./out', dataToWrite, 'utf8', function (err) {});
+    res.send();
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get("/file",function(req,res){
+    res.sendFile('/Users/zhuozeyu/CMU/BrowserSecurity/HW2/server/out');
+})
